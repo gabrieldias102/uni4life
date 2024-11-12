2a4b8ebee2e37368b6563afb48998b7f1e80e96f
Gabriel Dias <contatogbd@gmail.com>
2024-11-12 16:05:30 -0300

    adicionando criação de post

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,17 +1,25 @@
 import NavBar from "./components/NavBar";
 import ProfileCard from "./components/ProfileCard";
 import ConnectedUsersCard from "./components/ConnectedUsersCard";
+import CreatePostCard from "./components/CreatePostCard";
 
 function HomePage() {
   return (
     <div>
-      <header className="pb-8">
+      <nav className="pb-8">
         <NavBar />
-      </header>
-      <aside className="flex flex-col gap-16 px-4 h-full w-2/5">
-        <ProfileCard />
-        <ConnectedUsersCard />
-      </aside>
+      </nav>
+      <div className="flex flex-row">
+        <aside className="flex flex-col gap-16 pl-4 h-full w-2/5">
+          <ProfileCard />
+          <ConnectedUsersCard />
+        </aside>
+        <main className="flex flex-col gap-16 h-full w-full">
+          <CreatePostCard />
+        </main>
+
+        <aside className="flex flex-col gap-16 pr-4 h-full w-2/5"></aside>
+      </div>
     </div>
   );
 }
---
+++ src/components/CreatePostCard.tsx
@@ -0,0 +1,35 @@
+import { FaPaperclip } from "react-icons/fa";
+import { AiFillPicture } from "react-icons/ai";
+
+const CreatePostCard = () => {
+  return (
+    <div className="bg-secondaryColor border border-primaryColor rounded-3xl p-4 shadow-md">
+      <h2 className="text-teal-600 text-2xl font-bold mb-4">
+        Criar Publicação
+      </h2>
+
+      <textarea
+        placeholder="Escreva sua publicação...."
+        className="w-full bg-white border border-primaryColor rounded-xl p-2 text-primaryColor resize-none focus:outline-none focus:ring focus:ring-tertiaryColor"
+        rows={4}
+      ></textarea>
+
+      <div className="flex justify-between items-center mt-4">
+        <button className="bg-tertiaryColor text-secondaryColor px-4 py-2 rounded-2xl hover:bg-red-500 transition">
+          Publicar
+        </button>
+
+        <div className="flex space-x-3">
+          <button className="p-2 bg-tertiaryColor rounded-full text-secondaryColor hover:bg-red-500 transition">
+            <AiFillPicture size={20} />
+          </button>
+          <button className="p-2 bg-tertiaryColor rounded-full text-secondaryColor hover:bg-red-500 transition">
+            <FaPaperclip size={20} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePostCard;
