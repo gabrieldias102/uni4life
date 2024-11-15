016894fda9b577e4ba6ab87fc6c1e720ede5e663
Vinicius Gaspar <viniciusgasparprf@gmail.com>
2024-11-14 23:47:09 -0300

    Configurando routes para as páginas

---
+++ src/routes/HomePage.tsx
@@ -0,0 +1,48 @@
+import NavBar from "../components/NavBar";
+import ProfileCard from "../components/ProfileCard";
+import ConnectedUsersCard from "../components/ConnectedUsersCard";
+import CreatePostCard from "../components/CreatePostCard";
+import OrganizationCard from "../components/OrganizationCard";
+import SuggestedUsersCard from "../components/SuggestedUsersCard";
+import PostFeed from "../components/PostFeed";
+
+function HomePage() {
+  const posts = [
+    {
+      userName: "Usuário 3",
+      userHandle: "Usuario3",
+      content:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt eum id eaque facilis similique, labore atque expedita ut quaerat officia natus consectetur unde maiores sunt.",
+    },
+    {
+      userName: "Usuário 4",
+      userHandle: "Usuario4",
+      content: "Another example post for the feed!",
+    },
+  ];
+
+  return (
+    <div>
+      <nav className="pb-8">
+        <NavBar />
+      </nav>
+      <div className="flex flex-row">
+        <aside className="flex flex-col gap-8 pl-4 h-full w-2/5">
+          <ProfileCard />
+          <ConnectedUsersCard />
+        </aside>
+        <main className="flex flex-col gap-16 h-full w-full">
+          <CreatePostCard />
+          <PostFeed posts={posts} />
+        </main>
+
+        <aside className="flex flex-col gap-8 pl-8 pr-4 h-full w-2/5">
+          <OrganizationCard />
+          <SuggestedUsersCard />
+        </aside>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/routes/ProfilePage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../components/NavBar";
+
+function ProfilePage() {
+    return (
+        <div>
+            <nav className="pb-8">
+                <Navbar/>
+            </nav>
+        </div>
+    )
+}
+
+export default ProfilePage;
+
