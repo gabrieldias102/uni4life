6d49973fff8771c254a7d493dbff347c27f90667
Gabriel Dias <contatogbd@gmail.com>
2024-11-19 15:59:01 -0300

    ajustando servidor e realizando primeira consulta

--- src/app/components/ConnectedUsersCard.tsx
+++ src/app/components/ConnectedUsersCard.tsx
@@ -1,24 +1,21 @@
 import { GiPerson } from "react-icons/gi";
 import { useEffect, useState } from "react";
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+}
+
 const ConnectedUsersCard = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch("/api/getUsers/routes.tsx");
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        setUsers(data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    fetchData();
+    fetch("http://localhost:3333/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error("Error fetching users:", error));
   }, []);
 
   return (
@@ -28,12 +25,12 @@ const ConnectedUsersCard = () => {
       </h2>
 
       <ul className="space-y-3">
-        {users.map((user, index) => (
-          <li key={index} className="flex items-center">
+        {users.map((user) => (
+          <li key={user.id} className="flex items-center">
             <div className="p-2 bg-tertiaryColor text-secondaryColor rounded-full flex items-center justify-center">
               <GiPerson size={25} />
             </div>
-            <p className="ml-4 text-primaryColor font-medium">{user}</p>
+            <p className="ml-4 text-primaryColor font-medium">{user.name}</p>
           </li>
         ))}
       </ul>
