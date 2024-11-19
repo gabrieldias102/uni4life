1c5ff36c93e8734202e2c036c4d0f8f3f24a5975
Gabriel Dias <contatogbd@gmail.com>
2024-11-19 12:18:56 -0300

    iniciando rotas e ajustando nomes de pastas

--- src/index.js
+++ src/index.js
@@ -2,27 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import HomePage from "./routes/HomePage";
-import ProfilePage from "./routes/ProfilePage"
+import HomePage from "./app/pages/HomePage";
+import ProfilePage from "./app/pages/ProfilePage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage/>
+    element: <HomePage />,
   },
   {
     path: "Profile",
-    element: <ProfilePage/>
+    element: <ProfilePage />,
   },
-])
-
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
