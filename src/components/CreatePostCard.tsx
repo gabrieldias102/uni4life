3377f3666194fecc0e57a01cd7cb493a77e41cd3
Vinicius Gaspar <viniciusgasparprf@gmail.com>
2024-11-19 15:52:12 -0300

    Criando primeira versão da aba mobile de criação de publicações

--- src/components/CreatePostCard.tsx
+++ src/components/CreatePostCard.tsx
@@ -3,14 +3,14 @@ import { AiFillPicture } from "react-icons/ai";
 
 const CreatePostCard = () => {
   return (
-    <div className=" bg-secondaryColor border border-primaryColor rounded-3xl p-4 shadow-md">
+    <div className="bg-secondaryColor w-full h-full p-4 xl:border xl:border-primaryColor xl:rounded-3xl xl:shadow-md">
       <h2 className="text-teal-600 text-2xl font-bold mb-4">
         Criar Publicação
       </h2>
 
       <textarea
         placeholder="Escreva sua publicação...."
-        className="w-full bg-white border border-primaryColor rounded-xl p-2 text-primaryColor resize-none focus:outline-none focus:ring focus:ring-tertiaryColor"
+        className="w-full xl:h-auto h-4/5 bg-white border border-primaryColor rounded-xl p-2 text-primaryColor resize-none focus:outline-none focus:ring focus:ring-tertiaryColor"
         rows={4}
       ></textarea>
 
