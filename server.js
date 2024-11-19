13e4379fc7e694008b662657a24561aeef90d4d7
Gabriel Dias <contatogbd@gmail.com>
2024-11-19 20:36:20 -0300

    Revert "adicionando servidor em produção"
    This reverts commit d105de48bdb253039a50019d23894f1b9fa88d93.

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 const cors = require("cors");
 const express = require("express");
-const path = require("path"); // Import necessário para trabalhar com caminhos
 const { PrismaClient } = require("@prisma/client");
 
 const app = express();
 const prisma = new PrismaClient();
 
-// Configuração básica
 app.use(cors());
 app.use(express.json());
 
-// Servir os arquivos estáticos do React
-app.use(express.static(path.join(__dirname, "build")));
-
-// Endpoints da API
 app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany({
     take: 3,
@@ -22,11 +16,4 @@ app.get("/users", async (req, res) => {
   res.json(users);
 });
 
-// Redirecionar qualquer outra rota para o React
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
-// Iniciar o servidor
-const PORT = process.env.PORT || 3333;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+app.listen(3333, () => console.log("Acesse a API em http://localhost:3333"));
