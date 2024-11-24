21378a8d06c818dd9d04af7a7bbaf4d8e6b4fa1c
Vinicius Gaspar <viniciusgasparprf@gmail.com>
2024-11-24 20:36:12 -0300

    Ajustando espaçamento e fontes de SuggestedUsers

--- src/app/components/SuggestedUsersCard.tsx
+++ src/app/components/SuggestedUsersCard.tsx
@@ -69,23 +69,23 @@ const SuggestedUsersCard = () => {
 
   return (
     <div className="bg-secondaryColor border border-primaryColor rounded-3xl p-4 shadow-md max-w-sm mx-auto w-full">
-      <h2 className="text-teal-600 text-2xl font-bold mb-4">
+      <h2 className="text-teal-600 lg:text-xl 2xl:text-2xl font-bold mb-4">
         Usuários Sugeridos
       </h2>
 
       <ul className="space-y-2">
         {suggestedUsers.slice(0, 3).map((user) => (
-          <li key={user.uid} className="flex items-center justify-between">
-            <div className="flex items-center">
+          <li key={user.uid} className="flex items-center justify-around w-full">
+            <div className="flex items-center w-1/2">
               <div className="p-2 bg-tertiaryColor text-secondaryColor rounded-full flex items-center justify-center">
                 <FaUserAlt size={20} />
               </div>
-              <p className="ml-4 text-primaryColor font-medium">{user.name}</p>
+              <p className="ml-4 text-primaryColor font-medium lg:text-xs 2xl:text-base">{user.name}</p>
             </div>
 
             <button
               onClick={() => handleConnect(user.uid)}
-              className="bg-red-400 text-white px-4 py-2 rounded-2xl hover:bg-red-500 transition font-medium"
+              className="bg-red-400 text-white rounded-2xl hover:bg-red-500 transition font-medium w-2/5 lg:text-xs 2xl:text-base lg:px-2 2xl:px-4 py-2"
             >
               Conectar +
             </button>
@@ -100,3 +100,5 @@ const SuggestedUsersCard = () => {
   );
 };
 export default SuggestedUsersCard;
+
+
